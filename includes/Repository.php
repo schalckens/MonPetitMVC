be313907f99432cec24bd36d92144d1aaa27cafa
Valentine Schalckens <v.schalckens@gmail.com>
2021-11-16 01:19:26 +0100

    création de fichier TP3

---
+++ includes/Repository.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//Fournira les méthodes de bases de récuparation et données depuis la BD : finf, findAll ...
