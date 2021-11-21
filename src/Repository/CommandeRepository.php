688a427b8c32a029abdf36ac976513442a8544a7
Valentine Schalckens <v.schalckens@gmail.com>
2021-11-21 15:46:19 +0100

    Ajout commande repository et correction afichage commande / client si client n'existe pas

---
+++ src/Repository/CommandeRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace APP\Repository;
+
+use Tools\Repository;
+
+class CommandeRepository extends Repository {
+    
+    public function __construct($entity) {
+        parent::__construct($entity);
+    }
+}
